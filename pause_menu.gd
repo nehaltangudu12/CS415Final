7a95288ef72454f1ced9f6e0065a72e321acd87a
19713 <kevinchensunshine@gmail.com>
2025-04-21 15:29:04 -0500

    [Alpha] Working menu (kinda jank but works)

--- pause_menu.gd
+++ pause_menu.gd
@@ -3,29 +3,22 @@ extends Control
 func resume():
 	get_tree().paused = false
 	$AnimationPlayer.play_backwards("blur")
+	self.visible = false
 	
 func pause():
 	get_tree().paused = true 
 	$AnimationPlayer.play("blur")
 
-func testEsc():
-	if Input.is_action_just_pressed("escape") and get_tree().paused == false:
+func _input(ev):
+	if Input.is_key_pressed(KEY_ESCAPE):
 		pause()
-	elif Input.is_action_just_pressed("escape") and get_tree().paused == true:
-		resume()
-
-
-	
+		self.visible = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	# Start with the menu hidden
+	self.visible = false
 	$AnimationPlayer.play("RESET") # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	testEsc()
-
 
 func _on_resume_pressed() -> void:
 	resume() # Replace with function body.
