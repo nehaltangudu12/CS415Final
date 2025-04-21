16be907d357a1d7d2b2649220b1ac589fdfa931c
Kevin Chen <kevinlc3@illinois.edu>
2025-04-21 01:00:14 -0700

    Working dialogue system with dialogue manager

--- UI/DialogPlayer.gd
+++ UI/DialogPlayer.gd
@@ -2,49 +2,32 @@ extends CanvasLayer
 
 @export_file("*json") var scene_text_file: String
 
-var scene_text: Dictionary = {}
-var selected_text: Array = []
 var in_progress: bool = false
 
-@onready var background = $Background
-@onready var text_label = $TextLabel
-@onready var sprite = $Sprite2D
+@onready var text_label = $DialogueLabel
 
 func _ready():
-	background.visible = false
-	scene_text = load_scene_text()
 	SignalBus.connect("display_dialog", Callable(self, "on_display_dialog"))
+	DialogueManager.dialogue_ended.connect(_on_dialogue_finished)
 
-func load_scene_text():
-	if FileAccess.file_exists(scene_text_file):
-		var file = FileAccess.open(scene_text_file, FileAccess.READ)
-		var test_json_conv = JSON.new()
-		test_json_conv.parse(file.get_as_text())
-		return test_json_conv.get_data()
-
-func show_text():
-	text_label.text = selected_text.pop_front()
-
-func next_line():
-	if selected_text.size() > 0:
-		show_text()
-	else:
-		finish()
-
-func finish():
-	text_label.text = ""
-	background.visible = false
-	sprite.visible = false
+func _on_dialogue_finished(resource):
 	in_progress = false
 	get_tree().paused = false
-	
-func on_display_dialog(text_key):
+
+func on_display_dialog(text_key: String):
 	if in_progress:
-		next_line()
-	else:
-		get_tree().paused = true
-		background.visible = true
-		sprite.visible = true
-		in_progress = true		
-		selected_text = scene_text[text_key].duplicate()
-		show_text()
+		return		
+
+	var resource_path = "res://Assets/Dialogue/" + text_key
+	var resource = load(resource_path)
+
+	if not resource:
+		push_error("Failed to load dialogue resource: " + resource_path)
+		return
+	
+	in_progress = true
+	get_tree().paused = true  # ❗ prevent gameplay
+	
+	var balloon = await DialogueManager.show_dialogue_balloon(resource, "start")
+	balloon.process_mode = Node.PROCESS_MODE_ALWAYS
+	# Dialogue ends will automatically call _on_dialogue_finished()
