572aee3d46bd165ecb698bc4f8e871d52f08e0c5
kevinchensunshine <92841323+kevinchensunshine@users.noreply.github.com>
2025-04-24 14:08:12 -0700

    [Alpha] Notification System (#18)
    * Working notification system
    
    * [Alpha] Working notifs
    
    ---------
    
    Co-authored-by: 19713 <kevinchensunshine@gmail.com>

---
+++ UI/Notif/notif.gd
@@ -0,0 +1,19 @@
+class_name NotifManager extends CanvasLayer
+
+@onready var text_ballon: DialogueManagerExampleBalloon = $DialogueBalloon
+@onready var quit_indicator = $RichTextLabel
+
+func _ready() -> void:
+	quit_indicator.visible = false
+	text_ballon.visible = false
+
+func _input(ev):
+	if Input.is_key_pressed(KEY_K):
+		display_notif("Hello")
+
+func display_notif(text: String, duration := 5.0):
+	if text == null:
+		return
+
+	quit_indicator.visible = true
+	text_ballon.start(text, "start")
