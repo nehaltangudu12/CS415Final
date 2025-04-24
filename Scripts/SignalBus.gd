389565fe6bff0737e81658858fb26d755b30f042
19713 <kevinchensunshine@gmail.com>
2025-04-24 11:53:50 -0500

    Demo for in-game triggers from dialogue

--- Scripts/SignalBus.gd
+++ Scripts/SignalBus.gd
@@ -1,4 +1,4 @@
 extends Node
 
+# @region emittable signals
 signal display_dialog(text_key)
-	
--- World/DialogArea.gd
+++ World/DialogArea.gd
@@ -8,6 +8,7 @@ var area_active: bool = false
 
 func _input(event):
 	if area_active and event.is_action_pressed("ui_accept"):
+		exit_read()
 		SignalBus.emit_signal("display_dialog", text_key)
 		
 func show_read():
@@ -23,3 +24,19 @@ func _on_DialogArea_area_entered(_area):
 func _on_DialogArea_area_exited(_area):
 	area_active = false
 	exit_read()
+
+# @region custom signal hander for dialogue events
+func _ready():
+	DialogueManager.got_dialogue.connect(_on_got_dialogue)
+
+func flash_indicator(duration: float) -> void:
+	interact_indicator.visible = true
+	await get_tree().create_timer(duration).timeout
+	interact_indicator.visible = false
+
+func _on_got_dialogue(line: DialogueLine):
+	print(line.text)
+	if line.text == "It'll look something like this flashing on screen":
+		flash_indicator(5)
+		
+		
