f389a5691f1b9679f7c9d52c423df7092df21356
19713 <kevinchensunshine@gmail.com>
2025-04-29 00:10:02 -0500

    [Beta] Scene transitions and full room one gameplay

--- Levels/room_one.gd
+++ Levels/room_one.gd
@@ -6,9 +6,20 @@ extends Node2D
 var paused = false
 
 @onready var player = $PlayerCat
+@onready var bartender = $Bartender
+@onready var transition_area = $TransitionArea
+
+enum DISHES_WASHED_STATE {
+	DISHES_NOT_WASHED,
+	DISHES_IN_PROGRESS,
+	DISHES_WASHED,
+}
+
+static var dishes_washed_state: DISHES_WASHED_STATE = DISHES_WASHED_STATE.DISHES_NOT_WASHED
 
 func _ready():
 	SignalBus.open_inventory.connect(pauseMenu)
+	DialogueManager.passed_title.connect(on_finished_talking_with_nathan)
 	player.shrink_speed = 0.0
 	var light = player.get_node("PointLight2D")
 	light.energy = 0.5
@@ -18,6 +29,15 @@ func _input(event):
 	if event.is_action_pressed("inventory"):
 		pauseMenu()
 
+func change_dishes_washed_state(state: DISHES_WASHED_STATE):
+	dishes_washed_state = state
+	if state == DISHES_WASHED_STATE.DISHES_WASHED:
+		bartender.change_dialogue_key("bartender2.dialogue")
+	
+func on_finished_talking_with_nathan(title: String):
+	if title == "nathan_end":
+		await Notif.display_notif("Lemme get out of here, it's late... WTF, 9:00PM?", 5.0)
+		transition_area.set_can_use(true)
 	
 # Pause Logic
 func pauseMenu():
