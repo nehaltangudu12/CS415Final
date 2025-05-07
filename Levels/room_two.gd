59cddb830d11de47458b080e0302387a66334810
Tiffany Yu <yutiff25@gmail.com>
2025-05-07 14:36:21 -0500

    failed attempt to merge

--- Levels/room_two.gd
+++ Levels/room_two.gd
@@ -1,14 +1,22 @@
 extends Node2D
 
 @export var next_level:PackedScene
+@export var desired_phone_number = "3142713141"
 
+@onready var transition_area = $TransitionArea
+@onready var phone_booth = $PhoneBooth
 @onready var inv_menu = $Camera2D/InvMenu
 var paused = false
 
 @onready var player = $PlayerCat
-@onready var transition_area = $TransitionArea
 
 func _ready():
+	# Initial Level Setup
+	phone_booth.set_text_input_callback(_check_phone_number_matches)
+	await get_tree().create_timer(1.0).timeout
+	
+	Notif.display_notif("My car is gone... did I get towed? Let me give them a call at the phone booth. Wait, what is their number?")
+	
 	SignalBus.open_inventory.connect(pauseMenu)
 	player.shrink_speed = 0.01
 	var light = player.get_node("PointLight2D")
@@ -32,3 +40,12 @@ func pauseMenu():
 
 func _on_button_pressed() -> void:
 	SignalBus.SWAP_LEVELS.emit(next_level.get_path())
+
+func _check_phone_number_matches(val: String):
+	if val == desired_phone_number:
+		Notif.display_notif("Ring... Ring... Ring...")
+		await get_tree().create_timer(3).timeout
+		Notif.display_notif("They're not recieving. Looks like I'll have to take the bus home today")
+		transition_area.set_can_use(true)
+	else:
+		Notif.display_notif("Must be the wrong number...")
